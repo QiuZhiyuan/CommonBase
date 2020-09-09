55ca47274e2c5b0cb9c06546d16863a8ff033377
QiuZhiyuan <845026103@qq.com>
2020-09-09 14:26:10 +0800

    Task: add schedule

--- lib/src/main/java/com/qiu/base/lib/tools/sys/MobileInfoTools.java
+++ lib/src/main/java/com/qiu/base/lib/tools/sys/MobileInfoTools.java
@@ -3,6 +3,7 @@ package com.qiu.base.lib.tools.sys;
 import static android.content.pm.PackageManager.MATCH_UNINSTALLED_PACKAGES;
 
 import android.app.AppOpsManager;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
@@ -56,38 +57,5 @@ public class MobileInfoTools {
     @NonNull
     public static String getAppName(@NonNull PackageInfo packageInfo) {
         return packageInfo.applicationInfo.loadLabel(App.i().getPackageManager()).toString();
-    }
-
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public static boolean checkNotificationPermission(@NonNull String packageName) {
-        PackageManager pm = App.i().getPackageManager();
-        try {
-            AppOpsManager appOps = (AppOpsManager) App.i().getApplicationContext()
-                    .getSystemService(Context.APP_OPS_SERVICE);
-            ApplicationInfo appInfo =
-                    pm.getApplicationInfo(packageName, MATCH_UNINSTALLED_PACKAGES);
-            int uid = appInfo.uid;
-            Class<?> appOpsClass = Class.forName(AppOpsManager.class.getName());
-            Method checkOpNoThrowMethod = appOpsClass.getMethod(CHECK_OP_NO_THROW, Integer.TYPE,
-                    Integer.TYPE, String.class);
-            Field opPostNotificationValue = appOpsClass.getDeclaredField(OP_POST_NOTIFICATION);
-            int value = (int) opPostNotificationValue.get(Integer.class);
-            return ((int) checkOpNoThrowMethod.invoke(appOps, value, uid, packageName)
-                    == AppOpsManager.MODE_ALLOWED);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        return false;
     }
 }
